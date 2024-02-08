658b9098182cfb977a3cf15b2c0fd400cc66c5ce
Aidan Jones <aejones4gm@gmail.com>
2024-02-08 17:45:05 -0500

    add code from second attempt

---
+++ original.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+from PIL import Image
+from numpy import asarray
+from quad_image import quad_image, child_indices, size
+from os.path import getsize
+from os import remove
+from time import time
+
+def test_many():
+    files = [
+        "test-images/test.png",
+        "test-images/ball.png",
+        "test-images/Basketball.png",
+        "test-images/Curvature.png",
+        #"test-images/Dragon.png",
+        "test-images/HighRappel.png",
+        "test-images/Room.png",
+        "test-images/stone.png",
+        "test-images/StoneTile.png",
+        "test-images/test2.png",
+        "test-images/test3.png",
+        "test-images/worst-square4.png",
+        "test-images/worst-square64.png"
+    ]
+
+    thresholds = [0,1,2,4,8,16,32,64]
+
+    for th in thresholds:
+        print("threshold: "+str(th))
+        for f in files:
+            print(f)
+            img = Image.open(f)
+            png = getsize(f)
+            npdata = asarray(img)
+            print(" dimensions: "+str(npdata.shape))
+            print(" PNG       : "+str(png)+" bytes")
+            dims = npdata.shape
+            raw = dims[0] * dims[1] * dims[2]
+            print(" raw       : "+str(raw)+" bytes")
+            t = time()
+            qi = quad_image(npdata, th)
+            print(" construct : "+str(time() - t))
+            t = time()
+            qi.write_file("aaa")
+            print(" file      : "+str(time() - t))
+            qf = getsize("aaa")
+            print(" quad-image: "+str(qf)+" bytes")
+            
+            t = time()
+            qi.compress()
+            print(" compress  : "+str(time() - t))
+            t = time()
+            qi.write_file("bbb")
+            print(" file      : "+str(time() - t))
+            cmp = getsize("bbb")
+            print(" compressed: "+str(cmp)+" bytes")
+
+            remove("aaa")
+
+            print(" qi/raw    : "+str(qf/raw))
+            print(" cmp/raw   : "+str(cmp/raw))
+            print(" cmp/png   : "+str(cmp/png))
+
+            q2 = quad_image()
+            t = time()
+            q2.read_file("bbb")
+            print(" read      : "+str(time() - t))
+            remove("bbb")
+            t = time()
+            q2.decompress()
+            print(" dcmp      : "+str(time() - t))
+            nd2 = q2.get_channels()
+            pi = Image.fromarray(nd2)
+            print(str(th)+"-"+f)
+            pi.save(str(th)+"-"+f)
+
+def test_one():
+    fname = "test-images/room"
+    ext = "png"
+    img = Image.open(fname + "." + ext)
+    npdata = asarray(img)
+    png = getsize(fname + "." + ext)
+    print(" dimensions: "+str(npdata.shape))
+    print(" PNG       : "+str(png)+" bytes")
+    dims = npdata.shape
+    raw = dims[0] * dims[1] * dims[2]
+    print(" raw       : "+str(raw)+" bytes")
+    t = time()
+    qi = quad_image(npdata)
+    print(" construct : "+str(time() - t))#47 seconds
+    t = time()
+    qi.write_file(fname + "." + ext + ".qi")#310 seconds
+    print(" file      : "+str(time() - t))
+    qf = getsize(fname + "." + ext + ".qi")
+    print(" quad-image: "+str(qf)+" bytes")
+
+    t = time()
+    qi.compress()
+    print(" compress  : "+str(time() - t))#233 seconds
+    t = time()
+    qi.write_file(fname + "-c." + ext + ".qi") #5860 seconds
+    print(" file      : "+str(time() - t))
+    cmp = getsize(fname + "-c." + ext + ".qi")
+    print(" compressed: "+str(cmp)+" bytes")
+    
+    q2 = quad_image()
+    q2.read_file(fname + "-c." + ext + ".qi")
+    q2.decompress()
+    nd2 = q2.get_channels()
+    pi = Image.fromarray(nd2)
+    pi.save(fname + "-s." + ext)
+
+if __name__=="__main__":
+    test_one()
---
+++ quad_image.py
@@ -0,0 +1,156 @@
+import numpy as np
+from math import log2, floor
+from functools import reduce
+
+def p2(a: int) -> int:
+    b = log2(a)
+    c = floor(b)
+    return c if b.is_integer() else c + 1
+
+def depth(x: int, y: int) -> int:
+    return max(p2(x), p2(y))
+
+def size(levels: int) -> int:
+    return (1 - 4 ** (levels + 1)) // (-3)
+
+def child_indices(p: int, l: int):
+    #return [i for i in range(4*p + 1, 4*p + 5)]
+    o = 2**l
+    i = p - size(l - 1)
+    j = i + size(l)
+    j += (i % o)
+    j += (2*l + 2**(l+1)) * (i//o)
+
+    return (j, j+1, j + 2**(l+1), j + 2**(l+1) + 1)
+
+class quad_image:
+    def __init__(this, arr: np.ndarray=np.ndarray((0,0,0)), threshold: int=0):
+        dims = arr.shape
+        if dims[0] <= 0:
+            return
+        this.w = dims[1] #actual width of image
+        this.h = dims[0] #actual height of image
+        this.depth = depth(this.w, this.h) #depth of tree
+        this.size = size(this.depth) #size of tree
+        this.x = 2**this.depth #adjusted width of image for compression
+        this.channels = np.ndarray((dims[2], this.size), np.uint8) #array of sequential quadtrees
+        this.ch_flags = np.zeros((dims[2], 1 + this.size // 8), dtype=np.uint8) #bit flags of node vs leaf for compression
+        fill = this.size - this.x ** 2
+        for c in range(dims[2]):
+            for i in range(fill):
+                this.channels[c][i] = 0
+            for x in range(this.x):
+                for y in range(this.x):
+                    if y >= this.w or x >= this.h:
+                        this.channels[c][fill + x * this.x + y] = this.channels[c][fill + x * this.x + y - 1]#0
+                    else:
+                        this.channels[c][fill + x * this.x + y] = arr[x][y][c]
+            this.ch_flags[c][1 + fill//8:] = 0xff
+            this.ch_flags[c][fill//8] |= 0xff >> fill % 8
+            this.ch_flags[c][this.size//8] &= 0xff << 8 * this.ch_flags.shape[1] - this.size
+        this.threshold = threshold
+
+    def isleaf(this, ch, ni):
+        return ni < this.size and (this.ch_flags[ch][ni//8] & 0x80 >> ni%8) != 0
+    
+    def flip(this, ch, ni):
+        this.ch_flags[ch][ni//8] ^= 0x80 >> ni%8
+
+    def cmp_help(this, ch: int, ri: int, lev: int):
+        if ri >= this.size:
+            return ri
+        children = child_indices(ri, lev)
+        for c in children:
+            this.cmp_help(ch, c, lev + 1)
+        for c in children:
+            if not this.isleaf(ch, c):
+                return ri
+        avg = 0
+        for c in children:
+            avg += this.channels[ch][c]
+        avg //= 4
+        for c in children:
+            if abs(avg - this.channels[ch][c]) > this.threshold:
+                return ri
+        this.channels[ch][ri] = avg
+        this.flip(ch, ri)
+        for c in children:
+            this.flip(ch, c)
+        return ri
+
+    def compress(this):
+        for c in range(this.channels.shape[0]):
+            #print("before: ")
+            #print(this.ch_flags[c])
+            #print(list(this.channels[c]))
+            this.cmp_help(c, 0, 0)
+            #print("after:")
+            #print(this.ch_flags[c])
+            #print(list(this.channels[c]))
+
+    def dcp_help(this, ch: int, ri: int, lev: int):
+        children = child_indices(ri, lev)
+        if ri >= this.size or children[0] >= this.size:
+            return ri
+        if this.isleaf(ch, ri):
+            this.flip(ch, ri)
+            for c in children:
+                this.channels[ch][c] = this.channels[ch][ri]
+                this.flip(ch, c)
+        for c in children:
+            this.dcp_help(ch, c, lev + 1)
+        return ri
+
+    def decompress(this):
+        for c in range(this.channels.shape[0]):
+            this.dcp_help(c, 0, 0)
+
+    def read_file(this, fname: str):
+        with open(fname, "rb") as f:
+            data = np.fromfile(f, dtype=np.uint8)
+            this.w = int.from_bytes(data[0:4], 'little')
+            this.h = int.from_bytes(data[4:8], 'little')
+            this.depth = depth(this.w, this.h) #depth of tree
+            this.size = size(this.depth) #size of tree
+            this.x = 2**this.depth #adjusted width of image for compression
+            ch = data[8]
+            this.channels = np.zeros((ch, this.size), dtype=np.uint8) #array of sequential quadtrees
+            this.ch_flags = np.zeros((ch, 1 + this.size // 8), dtype=np.uint8) #bit flags of node vs leaf for compression
+            start = 9
+            fill = this.size - this.x ** 2
+            for c in range(ch):
+                for i in range(this.size):
+                    if i % 8 == 0:
+                        this.ch_flags[c][i//8] = data[start]
+                        start += 1
+                    if i >= fill and ((i-fill) >= (this.x * this.h) or ((i - fill) % this.x) >= this.w):
+                        this.channels[c][i] = this.channels[c][i-1]
+                    elif this.isleaf(c, i):
+                        this.channels[c][i] = data[start]
+                        start += 1
+
+    def write_file(this, fname: str):
+        with open(fname, "wb") as f:
+            f.write(this.w.to_bytes(4, 'little'))
+            f.write(this.h.to_bytes(4, 'little'))
+            f.write(this.channels.shape[0].to_bytes(1))
+            fill = this.size - this.x ** 2
+            for c in range(this.channels.shape[0]):
+                i = 0
+                while i < this.size:
+                    if i % 8 == 0:
+                        f.write(this.ch_flags[c][i//8])
+                    if this.isleaf(c, i) and (i < fill or ((i-fill) < (this.x * this.h) and ((i - fill) % this.x) < this.w)):
+                        f.write(np.uint8(this.channels[c][i]))
+                    i += 1
+
+    def get_channels(this):
+        ret = np.ndarray((this.h, this.w, this.channels.shape[0]), np.uint8)
+        dims = ret.shape
+        mi = this.size - this.x ** 2
+        for c in range(dims[2]):
+            for x in range(this.x):
+                for y in range(this.x):
+                    if y < this.w and x < this.h:
+                        ret[x][y][c] = this.channels[c][mi + x * this.x + y]
+        return ret
